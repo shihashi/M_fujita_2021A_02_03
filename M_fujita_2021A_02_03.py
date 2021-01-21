55b1e25458ffc9a42c8cadda00c73fbd10f27364
shihashi <hashi@mebio.co.jp>
2021-01-21 19:35:08 +0900

    first commit

---
+++ M_fujita_2021A_02_03.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# M_fujita_2021A_02_03.py
+#
+# Copyright (c) 2021 shihashi
+#
+# Released under the MIT license.
+# see https://opensource.org/licenses/mit-license.php
+#
+
+import itertools
+from math import gcd
+
+itemList = ['1', '2', '11', '12']
+numRepeat = 3
+
+
+def main():
+    print(len({int(''.join(x)) for x in itertools.product(itemList, repeat = numRepeat)}))
+
+
+if __name__ == '__main__':
+    main()
